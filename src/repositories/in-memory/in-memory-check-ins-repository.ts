5e44a3c9a6ad242d9b29f9e0fe38f0455e16af44
gabrielvrl <gabrielvarelaneto11@gmail.com>
2023-10-17 18:29:26 -0300

    Caso de uso de historico

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -4,6 +4,7 @@ import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+
   public items: CheckIn[] = [];
   
   async findByUserIdOnDate(userId: string, date: Date) {
@@ -25,6 +26,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkOnSameDate
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items.filter((item) => item.user_id === userId).slice((page -1) * 20, page * 20)
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn =  {
       id: randomUUID(),
@@ -38,4 +43,5 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkIn
   }
+
 }
