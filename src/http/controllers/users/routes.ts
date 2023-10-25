8346746575aa114c158b0c6feb512338ad90a10b
gabrielvrl <gabrielvarelaneto11@gmail.com>
2023-10-25 15:12:55 -0300

    Controllers das rotas de check-ins

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,12 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
-import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', { onRequest: [verifyJWT] }, profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
